fa3146e447b811fb1f8d7a0d1bbca231aa8d9f97
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2025-05-26 21:32:13 -0400

    added security3

--- src/components/Keycloack/keycloak.js
+++ src/components/Keycloack/keycloak.js
@@ -2,7 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://keycloak:8080/', // l'URL de Keycloak
+  url: 'http://localhost:8080/', // l'URL de Keycloak
   realm: 'stock-realm',
   clientId: 'app-stock',
 });
